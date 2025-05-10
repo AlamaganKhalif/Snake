9f1cdccfc6bc1e6a82a9ded3fadf93df4018069a
Alamagan Ke <ali_shumdar@hotmail.com>
2025-05-10 02:34:45 +0100

    done the snake and the apple have to debug

--- scripts/app.js
+++ scripts/app.js
@@ -1,69 +1,99 @@
-const createGrid = document.querySelector(".grid")
+const grid = document.querySelector(".grid")
 const userScore = document.querySelector(".score")
-const width = 20
+const statusDisplay = document.querySelector(".status")
+const width = 10
 const cellblock = width * width 
 const cells = []
 let score = 0
 let snake = [0]
 let direction = 1;
+let appleIndex = 0
+let gameInterval;
 
 
 // fuction to create grid 
 function createGrid (){
-    for (let i = 0; i < cellblock; 1++ ) {
+    for (let i = 0; i < cellblock; i++ ) {
         const cell = document.createElement("div");
-        cell.textContent = 1;
-      cells.puxh(cell);
+        cell.textContent = i;
+      cells.push(cell);
       grid.appendChild(cell)
     }
 }
 
 
-function beginGame() {
+function StartGame() {
 snake.forEach(index => cells[index].classList.remove("snake"))
 cells[appleIndex].classList.remove("apple")
+clearInterval(gameInterval)
 
 
-scoreDisplay.textContent = score;
+
+userScore.textContent = score
 statusDisplay.textContent = "";
 
 snake.forEach(index => cells[index].classList.add("snake"))
-}
 
+generateApple()
+gameInterval = setInterval(move, 200)
+
+}
 
 
 function move(){
     const head = snake[0]
-    
-    // check for collson 
-    if(
-        (direction === 1 && head % width === width -1) ||
-        (direction === -1 && head % width === 0) ||
-        (direction === width && head + width >= cells.length) ||
-        (direction === -width && head - width < 0)
+    if (
+        (direction === 1 && head % width === width - 1) || // right wall
+        (direction === -1 && head % width === 0) || // left wall
+        (direction === width && head + width >= cellblock) || // bottom
+        (direction === -width && head < width) || // top
+        cells[head + direction].classList.contains("snake") // self
     ) {
-
-        statusDisplay.textContent = "Game Over";
-        return;
-    }
-    cells[newHead].classList.add("snake")
-}
-
-const tail = snake.pop()
-cells[tali].classList.remove("snake");
-
-const newHead = head + direction
-snake.unshift(newHead);
-
-if (newHead === appleIndex) {
-    cells[appleIndex].classList.remove("apple")
-    snake.push(tail)
-    score++
-    score.scoreDisplay.textContent = score
-    generateApple();
-    if (snake.length === cellblock) {
-        statusDisplay.textContent = "You Win";
+        statusDisplay.textContent = "Game Over"
         clearInterval(gameInterval)
-        return 
-    }
+        return
+    }    
+
+   const tail = snake.pop()
+   cells[tail].classList.remove("snake")
+
+   const newHead = head + direction
+   snake.unshift(newHead)
+
+
+   if (newHead === appleIndex){
+    cells[appleIndex].classList.remove("apple")
+    snake.push(tail);
+    score++
+    userScore.textContent = score
+    generateApple()
+    if (snake.length === cellblock) {
+        statusDisplay.textContent = "You Win"
+        clearInterval(gameInterval)
+        return
+    }}
+ cells[newHead].classList.add("snake")
 }
+
+
+
+function generateApple(){
+    do {
+        appleIndex = Math.floor(Math.random() * cellsblock);
+    } while (cells[appleIndex].classList.contains("snake"))
+        cells[appleIndex].classList.add("apple")
+}
+
+
+
+function control(move) {
+    if (move.key === "ArrowRight" && direction !== -1) direction = 1;
+    else if (move.key === "ArrowUp" && direction !== width) direction = - width
+    else if (move.key === "ArrowLeft" && direction !== 1) direction = - 1
+    else if (move.key === "ArrowDown" && direction !== -width) direction =  width
+
+}
+
+document.addEventListener("keydown", control)
+createGrid()
+StartGame()
